f4e26d30379f2274a04014f7db2d4a89ac23ccab
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-02-26 15:34:11 +0300

    install redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {films} from './moks/films.js';
 import {reducer} from '../src/store/reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools()
+  );
 
 ReactDOM.render(
   <Provider store={store}>
