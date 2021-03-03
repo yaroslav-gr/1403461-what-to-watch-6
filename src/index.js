5681bf5e951acb4b2afbc0d7eb0082f1e129dd8f
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-03-03 17:23:55 +0300

    fix redux devtools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, compose} from 'redux';
 import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {films} from './moks/films.js';
 import {reducer} from '../src/store/reducer';
-import {composeWithDevTools} from 'redux-devtools-extension';
 
 const store = createStore(
     reducer,
-    composeWithDevTools()
+    compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
 );
 
 ReactDOM.render(
