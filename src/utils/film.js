3f6711c483478ccf5778d7b583c7ee7b536659ba
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-03-09 15:42:40 +0300

    create adapter

--- src/utils/film.js
+++ src/utils/film.js
@@ -7,3 +7,17 @@ export const formatRunTime = function (time) {
     return `0h ${time}m`;
   }
 };
+
+export const dataAdapter = (films) => {
+  const adaptedFilms = [];
+  films.map((film) => {
+    const newFilm = {};
+    const filmKeys = Object.keys(film);
+    const filmValues = Object.values(film);
+    filmKeys.map((key, index) => {
+      newFilm[key.replace(/_\w/g, (m) => m[1].toUpperCase())] = filmValues[index];
+    });
+    adaptedFilms.push(newFilm);
+  })
+  return adaptedFilms;
+};
