b41761881a691051f0fdc2cb837c328650927fe9
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-03-10 17:42:54 +0300

    loading-screen apdate

--- src/components/loading-screen/loading-screen.jsx
+++ src/components/loading-screen/loading-screen.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const LoadingScreen = () => {
   return (
-    <p>Loading ...</p>
+    <React.Fragment>
+      <div className="page-content" style={{height: `100vh`}}>
+        <div style={{fontSize: `30px`}}>Loading...</div>
+      </div>
+    </React.Fragment>
   );
 };
 
