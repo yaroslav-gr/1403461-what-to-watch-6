7e2a572d03ecc181b442abac31fae42cf1296af3
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-03-12 17:35:03 +0300

    update sing-inn component

--- src/components/sing-in/sing-in.jsx
+++ src/components/sing-in/sing-in.jsx
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Footer from '../footer/footer';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {login} from '../../store/api-actions';
 
-const SingIn = () => {
+const SingIn = ({onSubmit}) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    onSubmit({
+      login: emailRef.current.value,
+      password: passwordRef.current.value,
+    });
+  };
+
   return (
     <React.Fragment>
       <div className="user-page">
         <header className="page-header user-page__head">
           <div className="logo">
-            <a href="main.html" className="logo__link">
+            <Link to="/" className="logo__link">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
-            </a>
+            </Link>
           </div>
 
           <h1 className="page-title user-page__title">Sign in</h1>
         </header>
 
         <div className="sign-in user-page__content">
-          <form action="#" className="sign-in__form">
+          <form
+          action="#"
+          className="sign-in__form"
+          onSubmit={handleSubmit}>
             <div className="sign-in__fields">
               <div className="sign-in__field">
-                <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+
+                <input
+                ref={emailRef}
+                className="sign-in__input"
+                type="email"
+                placeholder="Email address"
+                name="user-email"
+                id="user-email" />
                 <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
               </div>
               <div className="sign-in__field">
-                <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+
+                <input
+                ref={passwordRef}
+                className="sign-in__input"
+                type="password"
+                placeholder="Password"
+                name="user-password"
+                id="user-password" />
                 <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
               </div>
             </div>
@@ -41,4 +73,10 @@ const SingIn = () => {
   );
 };
 
-export default SingIn;
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(SingIn);
