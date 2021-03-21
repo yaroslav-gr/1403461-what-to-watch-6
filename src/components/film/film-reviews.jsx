51237b7d3d1da4ccf398f0a182954dff2c9e7e83
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-03-22 00:02:22 +0300

    eslint

--- src/components/film/film-reviews.jsx
+++ src/components/film/film-reviews.jsx
@@ -7,11 +7,11 @@ const FilmReviews = ({reviews}) => {
     <React.Fragment>
       <div className="movie-card__reviews movie-card__row">
         <div className="movie-card__reviews-col">
-          {(reviews.slice(0,3)).map((review) => <ReviewItem review={review} key={review.id}/>)}
+          {(reviews.slice(0, 3)).map((review) => <ReviewItem review={review} key={review.id}/>)}
         </div>
 
         <div className="movie-card__reviews-col">
-          {(reviews.slice(3,6)).map((review) => <ReviewItem review={review} key={review.id}/>)}
+          {(reviews.slice(3, 6)).map((review) => <ReviewItem review={review} key={review.id}/>)}
         </div>
       </div>
     </React.Fragment>
