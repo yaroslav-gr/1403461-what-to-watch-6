3a69d93b886ea5e6333e837cce3e6ec73fe3348b
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-04-05 00:12:26 +0300

    testing error-films-loading

---
+++ src/components/error-loading/error-films-loading.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ErrorFilmsLoading from './error-films-loading';
+import {render, screen} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore({});
+let store;
+
+describe(`ErrorFilmsLoading should work correctly`, () => {
+  store = mockStore({
+    FILMS: {
+      errorMessage: `some message`
+    }
+  });
+
+  it(`ErrorFilmsLoading should render correctly`, () => {
+    render(
+      <Provider store={store}>
+        <ErrorFilmsLoading/>
+      </Provider>
+    );
+
+    expect(screen.getByText(/some message/i)).toBeInTheDocument();
+  });
+});
