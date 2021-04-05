eb6325cb08cd3213eb7134e14cce8d1dc16fb859
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-04-05 11:26:22 +0300

    testing add-favorite-button

---
+++ src/components/film/add-favorite-button.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import AddFavoriteButton from './add-favorite-button';
+import {render, screen} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
+import {AuthorizationStatus} from '../../const/const';
+
+const mockStore = configureStore({});
+let store;
+
+describe(`AddFavoriteButton should work correctly`, () => {
+  store = mockStore({
+    LOGIN: {
+      authorizationStatus: AuthorizationStatus.AUTH
+    }
+  });
+
+  it(`AddFavoriteButton should render correctly`, () => {
+    render(
+      <Provider store={store}>
+        <AddFavoriteButton id={5}/>
+      </Provider>
+    );
+
+    expect(screen.getByText(/My list/i)).toBeInTheDocument();
+  });
+});
