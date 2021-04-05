8f72f29e053a1ad7932546be28e25a3cb7ced98e
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-04-05 14:00:23 +0300

    testing main; add test-mock

---
+++ src/components/main/main.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Main from './main';
+import {render, screen} from '@testing-library/react';
+import {Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createMemoryHistory} from 'history';
+import configureStore from 'redux-mock-store';
+import {fakeFilms, fakeUserInfo} from '../../test/test-mocks/test-mocks';
+import {AuthorizationStatus} from '../../const/const';
+
+let store;
+let history;
+const mockStore = configureStore({});
+
+jest.mock(`../page-content/page-content`, () => {
+  const pageContent = () => <div>This is mock PageContent</div>;
+  pageContent.displayName = `PageContent`;
+  return {
+    __esModule: true,
+    default: () => {
+      return pageContent();
+    }
+  };
+});
+
+describe(`Main should work correctly`, () => {
+  history = createMemoryHistory();
+  store = mockStore({
+    FILMS: {
+      films: fakeFilms,
+    },
+    LOGIN: {
+      authorizationStatus: AuthorizationStatus.AUTH
+    },
+    USER: {
+      userInfo: fakeUserInfo
+    }
+  });
+
+  it(`Main should render correctly`, () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Main/>
+        </Router>
+      </Provider>
+    );
+
+    expect(screen.getByText(/Dardjeeling Limited/i)).toBeInTheDocument();
+    expect(screen.getByText(/This is mock PageContent/i)).toBeInTheDocument();
+    expect(screen.getByText(/fake@gmail.com/i)).toBeInTheDocument();
+  });
+});
---
+++ src/test/test-mocks/test-mocks.js
@@ -0,0 +1,26 @@
+export const fakeFilm = {
+  backgroundColor: `#AD9F8B`,
+  backgroundImage: `https://assets.htmlacademy.ru/intensives/javascript-3/film/background/Dardjeeling_Limited.jpg`,
+  description: `A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.`,
+  director: `Wes Anderson`,
+  genre: `Adventure`,
+  id: 3,
+  isFavorite: true,
+  name: `Dardjeeling Limited`,
+  posterImage: `https://assets.htmlacademy.ru/intensives/javascript-3/film/poster/Dardjeeling_Limited.jpg`,
+  previewImage: `https://assets.htmlacademy.ru/intensives/javascript-3/film/preview/dardjeeling_limited.jpg`,
+  previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  rating: 3.6,
+  released: 2007,
+  runTime: 91,
+  scoresCount: 165106,
+  starring: [`Owen Wilson`, `Adrien Brody`, `Jason Schwartzman`],
+  videoLink: `http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4`,
+};
+
+export const fakeFilms = new Array(5).fill(fakeFilm);
+
+export const fakeUserInfo = {
+  email: `fake@gmail.com`,
+  avatarUrl: `http://fake`
+};
