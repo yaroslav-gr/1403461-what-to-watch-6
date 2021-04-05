4c5501c4173e10799e7b6574d7b2670febf7166c
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-04-05 16:20:42 +0300

    testing show-more-button

---
+++ src/components/page-content/show-more-button.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import ShowMoreButton from './show-more-button';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+
+let store;
+const mockStore = configureStore({});
+
+describe(`ShowMoreButton should work correctly`, () => {
+  store = mockStore({});
+
+  it(`ShowMoreButton sgould render correctly`, () => {
+    render(
+      <Provider store={store}>
+        <ShowMoreButton/>
+      </Provider>
+    );
+
+    expect(screen.getByText(/Show more/i)).toBeInTheDocument();
+  });
+});
